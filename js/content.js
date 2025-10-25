acc43030603aab479e015869af3d0a2b3ed70149
Bisemy <morgan25012@gmail.com>
2025-10-25 13:27:09 +0800

    test

--- js/content.js
+++ js/content.js
@@ -167,7 +167,7 @@ export async function fetchLeaderboard() {
         let totalWithoutBonus = [verified, completed, progressed]
             .flat()
             .reduce((prev, cur) => prev + cur.score, 0);
-        const total = totalWithoutBonus - packScore + packScoreMultiplied
+        const total = round(totalWithoutBonus - packScore + packScoreMultiplied)
 
         return {
             user,
