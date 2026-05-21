4db6565f3479ace0f2d2d7948ed41818ea7dff1b
KrisYaYT <151280848+KrisYaYT@users.noreply.github.com>
2026-05-21 20:22:19 +0800

    test for points distribution

--- js/score.js
+++ js/score.js
@@ -19,7 +19,7 @@ export function score(rank) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-9.4865*Math.pow(rank-1, 0.6) + 100);
+    let score = (-9.4865*Math.pow(rank-1, 0.8) + 100);
 
     score = Math.max(0, score);
 
