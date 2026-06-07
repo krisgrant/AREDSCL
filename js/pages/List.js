629a01ca965ec5758e0da905798cf1d0a40a7632
Bisemy <morgan25012@gmail.com>
2026-06-07 12:24:56 +0000

    yes

--- js/pages/List.js
+++ js/pages/List.js
@@ -105,7 +105,7 @@ export default {
                     <br>
                     <p class="extended">June 6th 2026</p>
                     <br><br>
-                    <p><button class="btn-no-cover" @click="selected = 5">Ship challenge 4 has been removed (Formerly at #38). This change pushes KrisYas Bad Time back into the Extended List. This change is due to the approved removal of over 50% of server admins.
+                    <p><button class="btn-no-cover" @click="selected = 37">Ship challenge 4 has been removed (Formerly at #38). This change pushes KrisYas Bad Time back into the Extended List. This change is due to the approved removal of over 50% of server admins.
                     </div>
                     <div class="dark-bg">
                     <h2>Guidelines</h2>
