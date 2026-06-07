a594b8d76d258633db1aa9d6ed8437a998837b84
KrisYa <151280848+KrisYaYT@users.noreply.github.com>
2026-06-07 23:16:08 +0800

    test

--- js/pages/ListPacks.js
+++ js/pages/ListPacks.js
@@ -1,6 +1,7 @@
 import { fetchPacks, fetchPackLevels, fetchList } from "../content.js";
 import { embed } from "../util.js";
 import { score } from "../score.js";
+import { store } from "../main.js"; // ✅ FIX #1
 
 import Spinner from "../components/Spinner.js";
 import LevelAuthors from "../components/List/LevelAuthors.js";
@@ -10,85 +11,133 @@ export default {
         Spinner,
         LevelAuthors,
     },
+
     template: `
         <main v-if="loading">
             <Spinner></Spinner>
         </main>
+
         <main v-else class="pack-list">
             <div class="packs-nav">
                 <div>
-                    <button @click="switchLevels(i)" v-for="(pack, i) in packs" :style="{background: pack.colour}">
-                        <p>{{pack.name}}</p>
+                    <button
+                        v-for="(pack, i) in packs"
+                        :key="pack.name"
+                        @click="switchLevels(i)"
+                        :style="{background: pack.colour}"
+                    >
+                        <p>{{ pack.name }}</p>
                     </button>
                 </div>
             </div>
+
             <div class="list-container">
                 <table class="list" v-if="selectedPackLevels">
-                    <tr v-for="(level, i) in selectedPackLevels">
+                    <tr v-for="(level, i) in selectedPackLevels" :key="i">
                         <td class="rank">
-                            <p class="type-label-lg">#{{list.findIndex((lvl) => lvl[0].name == level[0].level.name) + 1}}</p>
+                            <p class="type-label-lg">
+                                #{{ list?.findIndex(lvl => lvl?.[0]?.name == level?.[0]?.level?.name) + 1 }}
+                            </p>
                         </td>
-                        <td class="level" :class="{ 'active': selectedLevel == i, 'error': !level }">
-                            <button :style= "[selectedLevel == i ? {background: pack.colour} : {}]" @click="selectedLevel = i">
-                                <span class="type-label-lg">{{ level[0].level.name || \`Error (\.json)\` }}</span>
+
+                        <td class="level" :class="{ active: selectedLevel == i, error: !level }">
+                            <button
+                                :style="selectedLevel == i ? {background: pack?.colour} : {}"
+                                @click="selectedLevel = i"
+                            >
+                                <span class="type-label-lg">
+                                    {{ level?.[0]?.level?.name || 'Error' }}
+                                </span>
                             </button>
                         </td>
                     </tr>
                 </table>
             </div>
+
             <div class="level-container">
-                <div class="level" v-if="selectedPackLevels[selectedLevel]">
+                <div class="level" v-if="selectedPackLevels?.[selectedLevel]">
                     <h1>{{ selectedPackLevels[selectedLevel][0].level.name }}</h1>
-                    <LevelAuthors :author="selectedPackLevels[selectedLevel][0].level.author" :creators="selectedPackLevels[selectedLevel][0].level.creators" :verifier="selectedPackLevels[selectedLevel][0].level.verifier"></LevelAuthors>
-                    <iframe class="video" :src="embed(selectedPackLevels[selectedLevel][0].level.verification)" frameborder="0"></iframe>
+
+                    <LevelAuthors
+                        :author="selectedPackLevels[selectedLevel][0].level.author"
+                        :creators="selectedPackLevels[selectedLevel][0].level.creators"
+                        :verifier="selectedPackLevels[selectedLevel][0].level.verifier"
+                    />
+
+                    <iframe
+                        class="video"
+                        :src="embed(selectedPackLevels[selectedLevel][0].level.verification)"
+                        frameborder="0"
+                    ></iframe>
+
                     <ul class="stats">
                         <li>
                             <div class="type-title-sm">ID</div>
-                            <p class="type-label-lg">{{ selectedPackLevels[selectedLevel][0].level.id }}</p>
+                            <p class="type-label-lg">
+                                {{ selectedPackLevels[selectedLevel][0].level.id }}
+                            </p>
                         </li>
+
                         <li>
                             <div class="type-title-sm">Skillset</div>
-                            <p>{{ selectedPackLevels[selectedLevel][0].level.skillset || 'Not Specified' }}</p>
+                            <p>
+                                {{ selectedPackLevels[selectedLevel][0].level.skillset || 'Not Specified' }}
+                            </p>
                         </li>
                     </ul>
+
                     <h2>Records</h2>
+
                     <table class="records">
-                        <tr v-for="record in selectedPackLevels[selectedLevel][0].level.records" class="record">
+                        <tr
+                            v-for="record in selectedPackLevels[selectedLevel][0].level.records"
+                            :key="record.user"
+                            class="record"
+                        >
                             <td class="percent">
-                                <p v-if="record.percent == 100"><b>{{ record.percent }}%</b></p>
-                                <p v-else>{{ record.percent }}%</p>
+                                <b v-if="record.percent == 100">{{ record.percent }}%</b>
+                                <span v-else>{{ record.percent }}%</span>
                             </td>
+
                             <td class="user">
-                                <a :href="record.link" target="_blank" class="type-label-lg">{{ record.user }}</a>
+                                <a :href="record.link" target="_blank">
+                                    {{ record.user }}
+                                </a>
                             </td>
+
                             <td class="hz">
                                 <p>{{ record.hz }}fps</p>
                             </td>
                         </tr>
                     </table>
                 </div>
+
                 <div v-else class="level" style="height: 100%; justify-content: center; align-items: center;">
                     <p>(ノಠ益ಠ)ノ彡┻━┻</p>
                 </div>
             </div>
+
             <div class="meta-container">
                 <div class="meta">
                     <div class="errors" v-show="errors.length > 0">
                         <p class="error" v-for="error of errors">{{ error }}</p>
                     </div>
+
                     <div class="dark-bg">
-                    <h1>Packs</h1>
-                    <br>
-                    <p>
-                    You can complete levels for these list packs, which have all been selected by the staff team, and have them added to your profile. Simply completing the levels is all that is necessary to have your records uploaded. When all levels are finished, the packs will instantly show up on your profile.
-                    </p>
+                        <h1>Packs</h1>
+                        <br>
+                        <p>
+                            You can complete levels for these list packs.
+                        </p>
                     </div>
+
                     <h3>Credits:</h3>
                     <p><a href="https://youtube.com/@krisgra" target="_blank">KrisGra</a></p>
                 </div>
             </div>
         </main>
     `,
+
     data: () => ({
         list: [],
         packs: [],
@@ -99,53 +148,51 @@ export default {
         loading: true,
         loadingPack: true,
     }),
+
     computed: {
         pack() {
-            return this.packs[this.selected];
+            return this.packs?.[this.selected] || null;
         },
     },
+
     async mounted() {
         this.packs = await fetchPacks();
+
+        // 🔥 FIX #2 safety guard
+        if (!this.packs?.length) {
+            this.loading = false;
+            return;
+        }
+
         this.list = await fetchList(store.mode);
-        console.log([...this.list])
-        console.log(
 
-        );
         this.selectedPackLevels = await fetchPackLevels(
-            this.packs[this.selected].name
+            this.packs[0].name
         );
 
-        // Error handling todo: make error handling
-        // if (!this.packs) {
-        //     this.errors = [
-        //         "Failed to load list. Retry in a few minutes or notify list staff.",
-        //     ];
-        // } else {
-        //     this.errors.push(
-        //         ...this.packs
-        //             .filter(([_, err]) => err)
-        //             .map(([_, err]) => {
-        //                 return `Failed to load level. (${err}.json)`;
-        //             })
-        //     );
-        // }
-
-        // Hide loading spinner
         this.loading = false;
         this.loadingPack = false;
     },
+
     methods: {
         async switchLevels(i) {
             this.loadingPack = true;
 
-            this.selected = i ;
+            this.selected = i;
             this.selectedLevel = 0;
+
+            if (!this.packs?.[i]) {
+                this.loadingPack = false;
+                return;
+            }
+
             this.selectedPackLevels = await fetchPackLevels(
-                this.packs[this.selected].name
+                this.packs[i].name
             );
 
             this.loadingPack = false;
         },
+
         score,
         embed,
     },
