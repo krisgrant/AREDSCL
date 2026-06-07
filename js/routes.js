63c0699b820c53660c7a6484f1d54d55d62e5f46
KrisYa <151280848+KrisYaYT@users.noreply.github.com>
2026-06-07 20:31:24 +0800

    tests

--- js/routes.js
+++ js/routes.js
@@ -8,4 +8,6 @@ export default [
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/list-packs', component: ListPacks },
+
+    { path: '/demons', component: List },
 ];
