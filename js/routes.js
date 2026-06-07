13ffed2155717b85414aabe7416bac43bcb1b94a
KrisYa <151280848+KrisYaYT@users.noreply.github.com>
2026-06-07 22:05:51 +0800

    test

--- js/routes.js
+++ js/routes.js
@@ -1,13 +1,18 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
-import ListPacks from './pages/ListPacks.js'
+import ListPacks from './pages/ListPacks.js';
 
 export default [
+    // CHALLENGE
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/list-packs', component: ListPacks },
 
+    // DEMON LIST
     { path: '/demons', component: List, meta: { list: 'demon' } },
+    { path: '/demons/leaderboard', component: Leaderboard, meta: { list: 'demon' } },
+    { path: '/demons/roulette', component: Roulette, meta: { list: 'demon' } },
+    { path: '/demons/list-packs', component: ListPacks, meta: { list: 'demon' } },
 ];
