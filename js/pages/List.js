580f05a48f7fc61d8f18b1b658328ed3e4e0cd27
KrisYa <151280848+KrisYaYT@users.noreply.github.com>
2026-06-21 15:31:34 +0800

    cobwebs

--- js/pages/List.js
+++ js/pages/List.js
@@ -161,8 +161,8 @@ export default {
                         <p class="extended">June 3rd 2026</p>
                         <br><br>
 
-                        <button class="btn-no-cover" @click="jumpTo(148)">
-                            BRUTAL has been lowered from #129 to #147, above Wavetrip and below Decay.
+                        <button class="btn-no-cover" @click="jumpTo(11)">
+                            Cobwebs has been beaten at #10, above Shardscapes and below RUST. This change pushes Motorbreath into the Extended List and niwa into the Legacy List.
                         </button>
                     </div>
 
