06070d016ddfaf87d3ee5e85c54351e35a7a90b1
Bisemy <morgan25012@gmail.com>
2026-08-06 14:35:37 +0000

    augus

--- js/pages/List.js
+++ js/pages/List.js
@@ -158,7 +158,7 @@ export default {
                     <div class="dark-bg" v-else>
                         <h2>Changelog:</h2>
                         <br>
-                        <p class="extended">July 31st 2026</p>
+                        <p class="extended">August 6th 2026</p>
                         <br><br>
 
                         <button class="btn-no-cover" @click="jumpTo(2)">
