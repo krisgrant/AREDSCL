a9ef379b8fed37c6ebda4f5a7802f58f46c930f8
Bisemy <morgan25012@gmail.com>
2026-08-08 15:32:06 +0000

    tuapeka

--- js/pages/List.js
+++ js/pages/List.js
@@ -158,11 +158,11 @@ export default {
                     <div class="dark-bg" v-else>
                         <h2>Changelog:</h2>
                         <br>
-                        <p class="extended">August 6th 2026</p>
+                        <p class="extended">August 8th 2026</p>
                         <br><br>
 
-                        <button class="btn-no-cover" @click="jumpTo(2)">
-                            Aerial Gleam has been placed at #3, above Zodiac and Below Coalescence. This change pushes moment into the Extended List and Lost Love into the Legacy List
+                        <button class="btn-no-cover" @click="jumpTo(111)">
+                            TUAPEKA has been placed at #112, above HURRICANE and below cerebral hemorrhage. This change pushes my worst level into the Legacy List.
                         </button>
                     </div>
 
