2bba04d37a560bc7d64ee9932163f9a14c79dc5e
Bisemy <morgan25012@gmail.com>
2026-08-22 04:40:25 +0000

    oblivion placement

--- js/pages/List.js
+++ js/pages/List.js
@@ -158,11 +158,11 @@ export default {
                     <div class="dark-bg" v-else>
                         <h2>Changelog:</h2>
                         <br>
-                        <p class="extended">August 18th 2026</p>
+                        <p class="extended">August 19th 2026</p>
                         <br><br>
 
-                        <button class="btn-no-cover" @click="jumpTo(149)">
-                            OVEDRIVE has been placed at #150, above Liz and below Forbidden Isle. This change pushes Liz into the Legacy List.
+                        <button class="btn-no-cover" @click="jumpTo(2)">
+                            Oblivion has been placed at #3, above Aerial Gleam and below Coalescence. This change pushes Carcano into the Extended List and OVERDRIVE into the Legacy List.
                         </button>
                     </div>
 
